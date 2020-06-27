3e589e364c9bdaa36be5824362d492e2be4d9971
Dylan Coomasaru <dc4915@gmail.com>
2020-06-27 01:30:16 -0400

    Updates

---
+++ precache-manifest.0c4864455cf99e696ca5607a757a9472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd3ddd58a09e35e3791e449da69746e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0a7320092757f754119",
+    "url": "/monsters-rolodex/static/css/main.2edde871.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a7320092757f754119",
+    "url": "/monsters-rolodex/static/js/main.15e1249a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
